54dc415b85bbf5cb027e9c011916eab9a0675639
Felipe Lleuful <felipe.lleuful@hotmail.com>
2026-08-19 21:19:21 -0400

    feat: agrega clase Calculadora con tests unitarios y actualiza a JUnit 5

--- src/test/java/cl/iplacex/automatizacion/AppTest.java
+++ src/test/java/cl/iplacex/automatizacion/AppTest.java
@@ -1,20 +1,25 @@
 package cl.iplacex.automatizacion;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+class AppTest {
+    
+    private final Calculadora calculadora = new Calculadora();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @DisplayName("Sumar 2 + 3 debe ser igual a 5")
+    void sumaDeDosEnterosPositivos() {
+        int resultado = calculadora.sumar(2, 3);
+        assertEquals(5, resultado);
+    }
+
+    @Test
+    @DisplayName("Restar 20 - 10 debe ser igual a 10")
+    void restaDeDosEnterosPositivos() {
+        int resultado = calculadora.restar(20, 10);
+        assertEquals(10, resultado);
     }
 }
